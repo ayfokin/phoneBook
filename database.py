3e0243910493dd6e27722fb9be829a559b36c416
ayfokin <ayfokin@mail.ru>
2021-07-25 23:59:16 +0300

    added insert+delete+edit methods to contact table

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 import mysql.connector
@@ -5,13 +6,15 @@ import mysql.connector
 
 class Database:
     def __init__(self):
+        with open("conf.json", "r") as config_file:
+            config = json.load(config_file)
         try:
             self.conn = mysql.connector.connect(
-                user="root",
-                password="rootroot",
-                host="localhost",
-                port=3306,
-                database="phone_book"
+                user=config["user"],
+                password=config["password"],
+                host=config["host"],
+                port=config["port"],
+                database=config["database"]
             )
         except mysql.connector.Error as e:
             print(e)
@@ -19,17 +22,22 @@ class Database:
         self.conn.autocommit = True
         self.cur = self.conn.cursor()
 
-    def _execute(self, query, variables):
+    def _execute(self, query, variables=None):
         self.cur.execute(query, variables)
 
     def addUser(self, username, password, email):
         self._execute("INSERT INTO users VALUES (%s, %s, %s)", (username, password, email))
 
     def addContact(self, name, phone, birthdate):
-        self._execute("INSERT INTO contacts VALUES (%s, %s, %date)", (name, phone, birthdate))
+        self._execute("INSERT INTO contacts VALUES (%s, %s, DATE %s)", (name, phone, birthdate))
 
     def deleteContact(self, name, phone, birthdate):
-        self._execute("DELETE FROM contacts WHERE name = %s AND phone = %s AND birthdate = %date", (name, phone, birthdate))
+        self._execute("DELETE FROM contacts WHERE name = %s AND phone = %s AND birthdate = DATE %s", (name, phone, birthdate))
+
+    def editContact(self, name, phone, birthdate, new_name, new_phone, new_birthdate):
+        self._execute("UPDATE contacts SET name = %s, phone = %s, birthdate = %s "
+                      "WHERE name = %s AND phone = %s AND birthdate = DATE %s",
+                      (new_name, new_phone, new_birthdate, name, phone, birthdate))
 
     def checkUser(self, username, password=None):
         if password is None:
@@ -37,4 +45,8 @@ class Database:
         else:
             self._execute("SELECT 1 FROM users WHERE username = %s AND password = %s", (username, password))
         return True if self.cur.fetchall() else False
+
+    def selectContacts(self):
+        self._execute("SELECT * FROM contacts ORDER BY name")
+        return self.cur.fetchall()
 
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # coding=utf-8
+import datetime
+import locale
+import os
 import sys
 import re
 
-from PyQt4.QtGui import QWidget, QMessageBox, QLineEdit, QMainWindow, QApplication, QDialog
+from PyQt4.QtCore import QDate, QTranslator, QLocale, QLibraryInfo, QCoreApplication
+from PyQt4.QtGui import QWidget, QMessageBox, QLineEdit, QMainWindow, QApplication, QDialog, QTableWidgetItem
 from interfaces.authorization import Ui_Authorization
 from interfaces.pwdRecovery import Ui_PwdRecovery
 from interfaces.mainScreen import Ui_MainScreen
@@ -40,7 +44,7 @@ class Login(QWidget, Ui_Authorization):
             self.close()
         else:
             msgbox = QMessageBox()
-            msgbox.critical(self, u"Ошибка", u"Пользователь с такими данными не найден")
+            msgbox.warning(self, u"Ошибка", u"Пользователь с такими данными не найден")
 
     def checkEnterButton(self):
         if self.username.text() != '' and self.pwd.text() != '':
@@ -87,15 +91,15 @@ class Registration(QWidget, Ui_Registration):
         email = str(self.email.text().toUtf8())
 
         if self.parent.isUserExists(username):
-            msgbox.critical(self, u"Ошибка", u"Пользователь с таким именем уже зарегистрирован")
+            msgbox.warning(self, u"Ошибка", u"Пользователь с таким именем уже зарегистрирован")
             return
 
         if password != r_password:
-            msgbox.critical(self, u"Ошибка", u"Пароли не совпадают")
+            msgbox.warning(self, u"Ошибка", u"Пароли не совпадают")
             return
 
         if not re.match(regex, email):
-            msgbox.critical(self, u"Ошибка", u"Введите корректный адрес электронной почты")
+            msgbox.warning(self, u"Ошибка", u"Введите корректный адрес электронной почты")
             return
 
         self.parent.database.addUser(username, password, email)
@@ -129,7 +133,7 @@ class PwdRecovery(QWidget, Ui_PwdRecovery):
         email = str(self.email.text().toUtf8())
 
         if not re.match(regex, email):
-            msgbox.critical(self, u"Ошибка", u"Введите корректный адрес электронной почты")
+            msgbox.warning(self, u"Ошибка", u"Введите корректный адрес электронной почты")
             return
         # sending email...
 
@@ -151,11 +155,77 @@ class MainScreen(QWidget, Ui_MainScreen):
         super(MainScreen, self).__init__()
         self.setupUi(self)
         self.parent = parent
-        # self.tableWidget.horizontalHeader().setFixedHeight(40)
+        self.tabsort = [u'Б', u'Г', u'Е', u'Й', u'Л', u'Н', u'П', u'С', u'У', u'Х', u'Щ', u'Э', u'Я']
+        self.contacts = [[] for _ in range(len(self.tabsort))]
 
-    def changeTable(self):
+        self.addContactButton.clicked.connect(self.insert)
+        self.editContactButton.clicked.connect(self.edit)
+        self.deleteContactButton.clicked.connect(self.delete)
+        self.listWidget.currentRowChanged.connect(self.updateTable)
+
+        self.tableWidget.horizontalHeader().setClickable(False)
+
+        contacts = self.parent.database.selectContacts()
+        index = 0
+        for contact in contacts:
+            self.insertContact(contact, index=index)
+        self.listWidget.setCurrentRow(0)
+        self.updateTable()
+
+    def insertContact(self, contact, sort=True, index=0):
+        letter = contact[0][0].upper()
+        while letter > self.tabsort[index]:
+            index += 1
+        if sort:
+            self.contacts[index].append(contact)
+        else:
+            left, right = 0, len(self.contacts[index])
+            while right - left > 0:
+                middle = (left + right) // 2
+                if self.contacts[index][middle][0].upper() > contact[0].upper():
+                    right = middle
+                else:
+                    left = middle + 1
+            self.contacts[index].insert(right, contact)
+
+    def editContact(self, contact):
+        self.deleteContact()
+        self.insertContact(contact, sort=False)
+
+    def deleteContact(self):
+        tab = self.listWidget.currentRow()
+        row = self.tableWidget.currentRow()
+        contact = self.contacts[tab][row]
+        self.parent.database.deleteContact(contact[0], contact[1], contact[2])
+        del self.contacts[tab][row]
+
+    def updateTable(self):
         index = self.listWidget.currentRow()
-        # self.stackedWidget.setCurrentIndex(index)
+        self.tableWidget.clearContents()
+        for i, contact in enumerate(self.contacts[index]):
+            for j, field in enumerate(contact):
+                if isinstance(field, datetime.date):
+                    field = QDate(field)
+                    field = field.toString("d MMMM yyyy")
+                self.tableWidget.setItem(i, j, QTableWidgetItem(field))
+
+    def insert(self):
+        self.insertContactDialog = InsertContact(self)
+        self.insertContactDialog.show()
+
+    def edit(self):
+        self.editContactDialog = EditContact(self)
+        self.editContactDialog.show()
+
+    def delete(self):
+        msgbox = QMessageBox()
+        reply = msgbox.question(self, u"Удаление", u"Вы действительно хотите удалить выбранный контакт?",
+                                msgbox.Yes, msgbox.No)
+        if reply == QMessageBox.Yes:
+
+            self.deleteContact()
+            self.updateTable()
+
 
 
 class ContactTemplate(QDialog, Ui_ContactTemplate):
@@ -163,22 +233,41 @@ class ContactTemplate(QDialog, Ui_ContactTemplate):
         super(ContactTemplate, self).__init__()
         self.setupUi(self)
         self.parent = parent
-        self.informationText = ''
 
-    def addContact(self):
-        name = str(self.username.text().toUtf8())
-        phone = str(self.phoneNumber.text().toUtf8())
-        birth = self.birthDate.dateTime()
+        self.okButton.clicked.connect(self.okButtonClicked)
+        self.cancelButton.clicked.connect(self.reject)
+
+    def okButtonClicked(self):
+        name = str(self.username.text().toUtf8()).decode("utf-8")
+        phone = str(self.phoneNumber.text().toUtf8()).decode("utf-8")
+        birth = self.birthDate.date()
         msgbox = QMessageBox()
 
-        regex = r'(^[\+\d]{1}\d{9}$)'
+        regex = ur'(^[А-Яа-я\s]+$)'
+        if not re.match(regex, name):
+            msgbox.warning(self, u"Ошибка", u"Имя может состоять только из символов кириллицы")
+            return
+        name = name.split()
+        name = ' '.join(name)
 
+        regex = r'(^\+?\d{11}$)'
         if not re.match(regex, phone):
-            msgbox.critical(self, u"Ошибка", u"Введите корректный номер телефона в формате +12345678900")
+            msgbox.warning(self, u"Ошибка", u"Введите корректный номер телефона в формате +12345678900")
             return
 
-        self.parent.database.addContact(name, phone, birth)
+
+        # TODO: edit after creating array of contacts
+        # if self.parent.database.checkContact(name, phone, birth):
+        #     msgbox.warning(self, u"Ошибка", u"Контакт с такими данными уже существует")
+        #     return
+
+        self.applyChanges(name, phone, birth)
         msgbox.information(self, u"Успех", self.informationText)
+        self.close()
+        self.parent.updateTable()
+
+    def applyChanges(self, name, phone, birth):
+        pass
 
 
 class InsertContact(ContactTemplate):
@@ -188,14 +277,24 @@ class InsertContact(ContactTemplate):
         self.title.setText(u"Добавить контакт")
         self.setWindowTitle(u"Добавление контакта")
 
+    def applyChanges(self, name, phone, birth):
+        self.parent.parent.database.addContact(name, phone, str(QDate(birth).toString("yyyy-MM-dd").toUtf8()).decode("utf-8"))
+        self.parent.insertContact((name, phone, birth.toPyDate()), sort=False)
+
 
 class EditContact(ContactTemplate):
     def __init__(self, parent):
         super(EditContact, self).__init__(parent)
         self.informationText = u"Контакт успешно изменен"
-        self.title.setText(u"Измененить контакта")
+        self.title.setText(u"Изменить контакт")
         self.setWindowTitle(u"Изменение контакта")
-        # TODO: add autofill for editText
+        self.username.setText(self.parent.tableWidget.selectedItems()[0].text())
+        self.phoneNumber.setText(self.parent.tableWidget.selectedItems()[1].text())
+        self.birthDate.setDate(QDate.fromString(self.parent.tableWidget.selectedItems()[2].text(), "d MMMM yyyy"))
+
+    def applyChanges(self, name, phone, birth):
+        self.parent.parent.database.editContact(name, phone, str(QDate(birth).toString("yyyy-MM-dd").toUtf8()).decode("utf-8"))
+        self.parent.editContact((name, phone, birth.toPyDate()))
 
 
 class PhoneBook(QMainWindow):
@@ -224,6 +323,12 @@ class PhoneBook(QMainWindow):
 
 
 app = QApplication(sys.argv)
+
+qt_translator = QTranslator(app)
+qt_translator.load("qt_" + QLocale().system().name(), QLibraryInfo.location(QLibraryInfo.TranslationsPath))
+app.installTranslator(qt_translator)
+
+
 window = PhoneBook()
 window.loginWindow()
 sys.exit(app.exec_())
